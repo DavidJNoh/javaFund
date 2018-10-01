3b200dd8f72ddf61a7c56db160439b17f70326c8
David Noh <davidnohj@gmail.com>
2018-10-01 11:23:52 -0700

    wer

---
+++ calculateHypotenuse.java
@@ -0,0 +1,7 @@
+public class calculateHypotenuse{
+    public static void main(String[] args){
+        Pythagorean solving = new Pythagorean ();
+        double result = solving.calculateHypotenuse(3,4);
+        System.out.println(result);
+    }
+}
